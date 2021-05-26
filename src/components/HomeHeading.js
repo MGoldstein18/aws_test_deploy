078b86e04026ae7c99b8d4f96dabe10f6dcbef49
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2021-05-26 21:33:25 +0200

    byline

--- src/components/HomeHeading.js
+++ src/components/HomeHeading.js
@@ -4,7 +4,7 @@ import "./homeHeading.css";
 function HomeHeading(props) {
   return (
     <p className="m-5 p-2" id="homeByLine">
-      Here is everything you need to know about the Blockchain in{" "}
+      Here is every question you need to know about the Blockchain answered in{" "}
       <b>
         <i>less than 30 seconds</i>
       </b>
